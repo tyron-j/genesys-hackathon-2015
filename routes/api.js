8b5fc404796bea0a56d835a64d0c9f04d3146a0f
James <f7jiang@uwaterloo.ca>
2015-09-27 02:06:58 -0400

    added loki logic

--- routes/api.js
+++ routes/api.js
@@ -1,12 +1,23 @@
 // deps
 var app    = require('../app.js');
 var socket = require('../socket.js').socket;
+var loki = require("lokijs");
 
 module.exports = {
 	getUser: function (req, res) {
-		// example
+		console.log("path: " + req.query.path);
+		
+		res.sendStatus(200);
 	}
 };
 
 // routes
 app.get('/api/user', getUser);
+
+var db = new loki('loki.json');
+
+var users = db.addCollection('users');
+
+users.insert({name:'Sleipnir', legs: 8});
+users.insert({name:'Jormungandr', legs: 0});
+users.insert({name:'Hel', legs: 2});
