8659611fb9b43c977e899fff23facc2bc34c6895
James <f7jiang@uwaterloo.ca>
2015-09-27 07:55:34 -0400

    added get last agent

--- routes/api.js
+++ routes/api.js
@@ -14,9 +14,11 @@ users.insert({
 	name:'Donald Trump',
 	calls: [
 		{
+			agent: "Chuang Li",
 			path: "TechSupport,TV"
 		},
 		{
+			agent: "Chuang Li",
 			path: "AccountChanges,Changes,Personal"
 		}
 	]
@@ -37,6 +39,10 @@ var emit; // emit function
 
 socket.on('connection', function (_socket) {
 	emit = _socket.emit.bind(_socket);
+	
+	_socket.on('agentLogin', function (res) {
+		
+	});
 });
 
 // api logic
@@ -57,6 +63,7 @@ var api = module.exports = {
 			var user = resultSet[0];
 			console.log(user);
 			user.calls.push({
+				agent: "Tyron Jung",
 				menuduration: parseInt(req.query.menuduration),
 				agentduration: agentDuration,
 				agentendtime: parseInt(req.query.menuendtime) + agentDuration,
@@ -73,6 +80,20 @@ var api = module.exports = {
 		console.log(sendObject);
 		emit('clientChange', sendObject);
 		res.sendStatus(200);
+	},
+	
+	getLast: function(req, res) {
+		var number = req.query.number.replace(' ', '+');
+		console.log(number);
+		var resultSet = users.find({number: number});
+		if (resultSet.length == 0){
+		
+		} else {
+		var callsList = users.find({number: number})[0].calls;
+		var lastCall = callsList[callsList.length - 1];
+		console.log(lastCall);
+		res.json(lastCall);
+		}
 	}
 };
 
@@ -80,7 +101,7 @@ var api = module.exports = {
 // routes
 app.get('/api/updateprofile', api.updateProfile);
 app.get('/api/changeclient', api.changeClient);
-
+app.get('/api/getlast', api.getLast);
 
 // refresh fix; DON'T WRITE BEYOND THIS
 app.get('*', views.index);
