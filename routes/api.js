37c31f1507ffd263c69108a02149090ee1d805a5
James <f7jiang@uwaterloo.ca>
2015-09-27 06:15:18 -0400

    changed duration

--- routes/api.js
+++ routes/api.js
@@ -42,10 +42,8 @@ socket.on('connection', function (_socket) {
 // api logic
 var api = module.exports = {
 	updateProfile: function (req, res) {
-		console.log(req.query.menuendtime);
-		console.log(req.query.menustarttime);
-		var menuDuration = req.query.menuendtime - req.query.menustarttime;
-		var agentDuration = req.query.agentendtime - req.query.menuendtime;
+		var menuDuration = parseInt(req.query.menuendtime) - parseInt(req.query.menustarttime);
+		var agentDuration = parseInt(req.query.agentendtime) - parseInt(req.query.menuendtime);
 		
 		var number = req.query.number.replace(' ', '+');
 		console.log("path: " + req.query.path, "number: " + number);
