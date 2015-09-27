9405ad2696395e15e18a2461b68142778545fb8b
James <f7jiang@uwaterloo.ca>
2015-09-27 02:34:56 -0400

    added rendering response stuff

--- routes/api.js
+++ routes/api.js
@@ -7,8 +7,7 @@ var loki = require("lokijs");
 var api = module.exports = {
 	getUser: function (req, res) {
 		console.log("path: " + req.query.path);
-		res.render(req.query.path);
-		res.sendStatus(200);
+		res.send(req.query.path);
 	}
 };
 
@@ -19,9 +18,8 @@ var db = new loki('loki.json');
 
 var users = db.addCollection('users');
 
-users.insert({name:'Sleipnir', legs: 8});
-users.insert({name:'Jormungandr', legs: 0});
-users.insert({name:'Hel', legs: 2});
+users.insert({name:'Donald Trump', legs: 3});
+users.insert({name:'Barack Obama', legs: 0});
 
 // refresh fix; DON'T WRITE BEYOND THIS
 app.get('*', views.index);
