957c46abe89de7b823d93f86c82a7c5bd4586cad
Tyron Jung <tyronjung@hotmail.com>
2015-09-27 02:42:50 -0400

    Inner Socket

--- socket.js
+++ socket.js
@@ -7,19 +7,19 @@ var app = require('./app.js');
 // set up socket
 var server = http.createServer(app);
 
-var socket = io(server).on('connection', function (socket) {
+var socket = io(server).on('connection', function (_socket) {
 	console.log("Client connected");
 
 	// test
 	setTimeout(function () {
 		console.log("Broadcasting...");
 
-		socket.broadcast.emit('clientChange', {
+		_socket.broadcast.emit('clientChange', {
 			hello: "world!"
 		});
 	}, 3000);
 
-	socket.on('disconnect', function () {
+	_socket.on('disconnect', function () {
 		console.log("Client disconnected");
 	});
 });
