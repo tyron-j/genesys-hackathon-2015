27260f4deccab98d89cf5795e7d76dad18f90c01
Chuang Li <chuang.li94@gmail.com>
2015-09-27 01:15:46 -0400

    basic layout of angent interface

--- public/js/app.js
+++ public/js/app.js
@@ -3,14 +3,22 @@ angular.module('myApp', [
 
 	'myApp.controllers'
 ]).config(function ($routeProvider, $locationProvider) {
-	$routeProvider.when('/client', {
-		templateUrl: 'partials/client',
-		controller: 'ClientCtrl'
-	}).otherwise({
-		redirectTo: '/client'
-	});
 
 	$locationProvider.html5Mode(true);
+
+	$routeProvider
+		.when('/', {
+			templateUrl: 'partials/client',
+			controller: 'ClientCtrl'
+		})
+		.when('/client', {
+			templateUrl: '/partials/client',
+			controller: 'ClientCtrl'
+		})
+		.otherwise({
+			redirectTo: '/client'
+		});
+
 
 	console.log("Heyyyy");
 });
--- public/js/controllers/clientctrl.js
+++ public/js/controllers/clientctrl.js
@@ -1,5 +1,36 @@
 angular.module('myApp.controllers').controller('ClientCtrl', [
-	function () {
+	'$scope',
+	function ($scope) {
 		console.log("ClientCtrl!");
+
+		$scope.firstname = "Tyron";
+		console.log($scope.firstname);
+		$scope.lastname = "Jiang";
+		$scope.location = "Toronto, Canada";
+
+		$scope.callList = [
+			{
+				"id": 1,
+				"duration" : 20000,
+				"timestamp" : "saturday"
+			},
+			{
+				"id": 2,
+				"duration" : 234254,
+				"timestamp" : "monday"
+			},
+			{
+				"id": 3,
+				"duration" : 86787,
+				"timestamp" : "tuesday"
+			},
+			{
+				"id": 4,
+				"duration" : 2345245,
+				"timestamp" : "sunday"
+			},
+		];
+		$scope.sortOrder = "id";
+
 	}
 ]);
